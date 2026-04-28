d9e11ae2a3dda66d3612d06b9b90b092daae64dc
bayyubenjamin <bayyujack@gmail.com>
2026-04-29 00:12:26 +0700

    refactor: streamline utility functions

--- src/utils.js
+++ src/utils.js
@@ -132,3 +132,4 @@
 // update 263 at Sel 28 Apr 2026 22:30:49 WIB
 // update 266 at Sel 28 Apr 2026 22:45:24 WIB
 // update 268 at Sel 28 Apr 2026 22:56:20 WIB
+// update 283 at Rab 29 Apr 2026 00:12:26 WIB
