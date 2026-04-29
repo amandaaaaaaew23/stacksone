a255e293405d2eec4c1ef4d592148c25b603bd9d
bayyubenjamin <bayyujack@gmail.com>
2026-04-29 16:15:25 +0700

    refactor: optimize contract execution flow

--- src/helper.js
+++ src/helper.js
@@ -125,3 +125,4 @@
 // update 280 at Sel 28 Apr 2026 23:55:57 WIB
 // update 1 at Rab 29 Apr 2026 13:11:50 WIB
 // update 21 at Rab 29 Apr 2026 14:58:11 WIB
+// update 36 at Rab 29 Apr 2026 16:15:25 WIB
