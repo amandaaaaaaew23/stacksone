6ae0df0c087cf305f00661edf93e8b27dffe5066
bayyubenjamin <bayyujack@gmail.com>
2026-05-01 08:58:09 +0700

    feat: introduce helper abstraction layer

--- src/App.jsx
+++ src/App.jsx
@@ -473,3 +473,4 @@ export default App;
 // update 35 at Jum 01 Mei 2026 05:25:25 WIB
 // update 39 at Jum 01 Mei 2026 05:43:25 WIB
 // update 40 at Jum 01 Mei 2026 05:49:29 WIB
+// update 77 at Jum 01 Mei 2026 08:58:09 WIB
