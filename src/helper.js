f18a2854b6e5da122be73b0c1f1b13dea1ccefd9
bayyubenjamin <bayyujack@gmail.com>
2026-05-03 19:47:39 +0700

    docs: improve architecture documentation clarity

--- src/helper.js
+++ src/helper.js
@@ -146,3 +146,4 @@
 // update 174 at Sab 02 Mei 2026 04:39:50 WIB
 // update 237 at Sab 02 Mei 2026 10:11:18 WIB
 // update 24 at Min 03 Mei 2026 19:09:09 WIB
+// update 31 at Min 03 Mei 2026 19:47:39 WIB
