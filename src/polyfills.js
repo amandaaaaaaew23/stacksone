6312481b37fe8a336dc11dc1ea419198276edeb7
bayyubenjamin <bayyujack@gmail.com>
2026-05-04 10:52:18 +0700

    docs: expand module overview documentation

--- src/polyfills.js
+++ src/polyfills.js
@@ -122,3 +122,4 @@ window.Buffer = Buffer;
 // update 181 at Sen 04 Mei 2026 08:22:55 WIB
 // update 185 at Sen 04 Mei 2026 08:42:22 WIB
 // update 201 at Sen 04 Mei 2026 10:04:43 WIB
+// update 4 at Sen 04 Mei 2026 10:52:18 WIB
