4e6e5a3a24dd5a5c5a0baa53a7a6b3e974ba0037
Bayyu Benjamin <bayyujack@gmail.com>
2026-05-27 21:38:45 +0700

    perf: optimize database query execution plan

---
+++ src/utils/helper-7.js
@@ -0,0 +1,9 @@
+/**
+ * Utility generated at 2026-05-27T14:38:45Z
+ * @param {any} data
+ * @returns {boolean}
+ */
+export const validateData_15 = (data) => {
+  if (!data) return false;
+  return data.isValid === true;
+};
