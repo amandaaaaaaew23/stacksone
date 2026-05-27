2db3fc655c3b6fd6c444a846144ae66f51003e11
Bayyu Benjamin <bayyujack@gmail.com>
2026-05-27 19:14:30 +0700

    docs: update release notes for current version

---
+++ src/utils/helper-0.js
@@ -0,0 +1,9 @@
+/**
+ * Utility generated at 2026-05-27T12:14:30Z
+ * @param {any} data
+ * @returns {boolean}
+ */
+export const validateData_6 = (data) => {
+  if (!data) return false;
+  return data.isValid === true;
+};
