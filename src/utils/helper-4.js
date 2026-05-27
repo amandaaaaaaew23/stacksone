bfeeb9e93ac7c7e6a200cc333f4a954b11999292
Bayyu Benjamin <bayyujack@gmail.com>
2026-05-28 04:01:53 +0700

    refactor: streamline container orchestration configurations

---
+++ src/utils/helper-4.js
@@ -0,0 +1,9 @@
+/**
+ * Utility generated at 2026-05-27T21:01:53Z
+ * @param {any} data
+ * @returns {boolean}
+ */
+export const validateData_41 = (data) => {
+  if (!data) return false;
+  return data.isValid === true;
+};
