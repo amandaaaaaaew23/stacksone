7c21806f92e51ac0ae74d06c9085c27ec477a6e5
Bayyu Benjamin <bayyujack@gmail.com>
2026-05-27 23:22:31 +0700

    docs: improve clarity of error codes documentation

---
+++ src/utils/helper-8.js
@@ -0,0 +1,9 @@
+/**
+ * Utility generated at 2026-05-27T16:22:31Z
+ * @param {any} data
+ * @returns {boolean}
+ */
+export const validateData_22 = (data) => {
+  if (!data) return false;
+  return data.isValid === true;
+};
