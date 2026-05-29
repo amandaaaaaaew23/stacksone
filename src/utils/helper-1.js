b73a22c13b1bb836f9cafdc7caa097ec974ab482
Bayyu Benjamin <bayyujack@gmail.com>
2026-05-29 20:29:53 +0700

    feat: add end-to-end encryption for direct messages

--- src/utils/helper-1.js
+++ src/utils/helper-1.js
@@ -1,9 +1,9 @@
 /**
- * Utility generated at 2026-05-28T07:51:03Z
+ * Utility generated at 2026-05-29T13:29:53Z
  * @param {any} data
  * @returns {boolean}
  */
-export const validateData_3 = (data) => {
+export const validateData_10 = (data) => {
   if (!data) return false;
   return data.isValid === true;
 };
