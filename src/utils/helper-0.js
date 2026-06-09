435aa8e273a45843a5e5815beae9485fe931c7d8
Bayyu Benjamin <bayyujack@gmail.com>
2026-06-09 17:41:40 +0700

    refactor: streamline Dockerfile build stages

--- src/utils/helper-0.js
+++ src/utils/helper-0.js
@@ -1,9 +1,9 @@
 /**
- * Utility generated at 2026-06-09T09:52:03Z
+ * Utility generated at 2026-06-09T10:41:40Z
  * @param {any} data
  * @returns {boolean}
  */
-export const validateData_21 = (data) => {
+export const validateData_24 = (data) => {
   if (!data) return false;
   return data.isValid === true;
 };
