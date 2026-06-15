c64f6aaf8bed520f3fb2ecd831caca85f6b44b84
Bayyu Benjamin <bayyujack@gmail.com>
2026-06-15 16:27:13 +0700

    perf: implement CDN caching rules

--- src/utils/helper-3.js
+++ src/utils/helper-3.js
@@ -1,9 +1,9 @@
 /**
- * Utility generated at 2026-06-14T22:30:53Z
+ * Utility generated at 2026-06-15T09:27:13Z
  * @param {any} data
  * @returns {boolean}
  */
-export const validateData_71 = (data) => {
+export const validateData_11 = (data) => {
   if (!data) return false;
   return data.isValid === true;
 };
