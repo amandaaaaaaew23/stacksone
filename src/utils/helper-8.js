64f579b96083a643cf3b3fabcef164c321c7d284
Bayyu Benjamin <bayyujack@gmail.com>
2026-06-29 23:02:41 +0700

    refactor: optimize asset loading strategy

--- src/utils/helper-8.js
+++ src/utils/helper-8.js
@@ -1,9 +1,9 @@
 /**
- * Utility generated at 2026-06-14T22:55:23Z
+ * Utility generated at 2026-06-29T16:02:41Z
  * @param {any} data
  * @returns {boolean}
  */
-export const validateData_73 = (data) => {
+export const validateData_20 = (data) => {
   if (!data) return false;
   return data.isValid === true;
 };
